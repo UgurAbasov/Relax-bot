9485a76f70d32c5df4b9281572aa587155f17b55
Ugur4Abbasov <89932220+Ugur4Abbasov@users.noreply.github.com>
2021-09-21 11:23:53 +0400

    Name

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord 
+from discord.ext import commands 
+bot = commands.Bot(command_prefix = ">", intents = discord.Intents.all())
+
+@bot.command(aliases = ['пинг'])
+async def ping(ctx):
+	await ctx.send("pong!")
+
+@bot.event
+async def on_ready():
+	print("I am connected!")
+
+@bot.event
+async def on_message_edit(before, after):
+	if before.content == after.content:
+		return
+	await before.channel.send(f"Сообщение было изменено!\n{before.content} -> {after.content}")
+
+bot.run("ODgyMjEwNjUyMDQxNjc0Nzky.YS4Esg.bWrL1GGJetS5xU0egZCgHI0CQt0")
